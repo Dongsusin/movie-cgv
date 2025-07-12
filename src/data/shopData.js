f99a85c5af2cc546b486fb1b93edba23279fde71
Dongsusin <auroratime020715@gmail.com>
2025-07-12 20:46:01 +0900

    디테일 페이지 수정

--- src/data/shopData.js
+++ src/data/shopData.js
@@ -1,47 +1,45 @@
-// src/data/shopData.js
-
 const shopItems = [
   {
     id: 1,
-    name: '고소한 팝콘',
-    image: 'http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421990122670.jpg',
-    price: 6500
+    name: "고소한 팝콘",
+    image: "http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421990122670.jpg",
+    price: 6500,
   },
   {
     id: 2,
-    name: '달콤한 팝콘',
-    image: 'https://img.cgv.co.kr/GiftStore/Product/Pc/List/17421988475900.jpg',
-    price: 7500
+    name: "달콤한 팝콘",
+    image: "https://img.cgv.co.kr/GiftStore/Product/Pc/List/17421988475900.jpg",
+    price: 7500,
   },
   {
     id: 3,
-    name: '바질어니언팝콘',
-    image: 'http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421986869910.jpg',
-    price: 7500
+    name: "바질어니언팝콘",
+    image: "http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421986869910.jpg",
+    price: 7500,
   },
   {
     id: 4,
-    name: '더블치즈팝콘',
-    image: 'http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421985136670.jpg',
-    price: 7500
+    name: "더블치즈팝콘",
+    image: "http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421985136670.jpg",
+    price: 7500,
   },
   {
     id: 5,
-    name: '탄산음료',
-    image: 'http://img.cgv.co.kr/GiftStore/Product/Pc/List/17422010262750.jpg',
-    price: 4000
+    name: "탄산음료",
+    image: "http://img.cgv.co.kr/GiftStore/Product/Pc/List/17422010262750.jpg",
+    price: 4000,
   },
   {
     id: 6,
-    name: '1인세트',
-    image: 'http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421944161320.jpg',
-    price: 8000
+    name: "1인세트",
+    image: "http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421944161320.jpg",
+    price: 8000,
   },
   {
     id: 7,
-    name: '2인세트',
-    image: 'http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421951864740.jpg',
-    price: 15000
+    name: "2인세트",
+    image: "http://img.cgv.co.kr/GiftStore/Product/Pc/List/17421951864740.jpg",
+    price: 15000,
   },
 ];
 
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,6 +1,6 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import movies from '../data/movies.json';
-import './MovieDetail.css';
+import { useParams, useNavigate } from "react-router-dom";
+import movies from "../data/movies.json";
+import "./MovieDetail.css";
 
 export default function MovieDetail() {
   const { id } = useParams();
@@ -13,36 +13,38 @@ export default function MovieDetail() {
   }
 
   const handleBooking = () => {
-    navigate('/booking', { state: { movie } });
+    navigate("/booking", { state: { movie } });
   };
 
   return (
     <div className="movie-detail">
-      <button onClick={() => navigate(-1)} className="back-button">← 뒤로가기</button>
+      <button onClick={() => navigate(-1)} className="back-button">
+        ← 뒤로가기
+      </button>
       <div className="movie-content">
         <div className="detail-info">
-          <div className='detail left'>
+          <div className="detail left">
             <h1>{movie.title}</h1>
-          <p><strong>평점:</strong> {movie.rating}</p>
-          <p><strong>장르:</strong> {movie.genre.join(', ')}</p>
-          <p><strong>개봉일:</strong> {movie.releaseDate}</p>
-          <p><strong>러닝타임:</strong> {movie.runningTime}분</p>
+            <p>
+              <strong>평점:</strong> {movie.rating}
+            </p>
+            <p>
+              <strong>장르:</strong> {movie.genre.join(", ")}
+            </p>
+            <p>
+              <strong>개봉일:</strong> {movie.releaseDate}
+            </p>
+            <p>
+              <strong>러닝타임:</strong> {movie.runningTime}분
+            </p>
           </div>
-          <div className='detail-right'>
+          <div className="detail-right">
             <img src={`/${movie.poster}`} alt={movie.title} />
             <button className="booking-button" onClick={handleBooking}>
-            예매하기
-          </button>
+              예매하기
+            </button>
           </div>
         </div>
-        <iframe
-            width="560"
-            height="315"
-            src={movie.trailer}
-            title="예고편"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
       </div>
     </div>
   );
